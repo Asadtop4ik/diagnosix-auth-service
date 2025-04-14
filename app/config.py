ae591e250bfefd52a75d4bb4a37e45540174d70c
Asadtop4ik <asadbek.backend@gmail.com>
2025-04-14 10:18:20 +0500

    bug configured

--- app/config.py
+++ app/config.py
@@ -1,7 +1,7 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    DATABASE_URL: str = "postgresql://postgres:password@localhost/auth_db"
+    DATABASE_URL: str = "postgresql://postgres:1234@localhost/auth_db"
     JWT_SECRET_KEY: str = "supersecret"
     JWT_ALGORITHM: str = "HS256"
 
